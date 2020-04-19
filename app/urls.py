4b2430f2ceb60bd4bc2a160313f7153ba749edb3
Nils Müller <shimst3r@gmail.com>
2020-04-19 20:06:23 +0200

    Remove depth from URLs.

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,6 @@ from .views import download_file, upload_file
 
 app_name = "app"
 urlpatterns = [
-    path("download/<uuid:blob_uuid>", download_file, name="download-file"),
-    path("upload/", upload_file, name="upload-file"),
+    path("<uuid:blob_uuid>", download_file, name="download-file"),
+    path("", upload_file, name="upload-file"),
 ]
